c508dd93a1f5441162832a459181df34b99b1f31
Caleb-Tan <calebtan23@gmail.com>
2018-07-10 22:24:35 -0700

    added fake headers

--- bs4_search.py
+++ bs4_search.py
@@ -8,7 +8,8 @@ from timeit import default_timer as timer
 url = 'https://google.com/search?q='
 
 def question(query):
-    response = requests.get(url + query)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(url + query, headers=headers)
     # with open('output2.html', 'wb') as f:
     #    f.write(response.content)
     # webbrowser.open('output2.html')
