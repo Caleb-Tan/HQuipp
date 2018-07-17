c284984d5908a2990c2eda383142e74578379d9f
Caleb-Tan <calebtan23@gmail.com>
2018-07-16 18:16:27 -0700

    question parsing update

--- question.py
+++ question.py
@@ -3,21 +3,21 @@ import asyncio
 
 # special keywords
 location_keywords = ["farthest", "closest", "furthest", "nearest"]
-location_keywords2 = ["city", "cities", "country", "countries", "building", "place", "state", "island", "location", "area"]
+location_keywords2 = ["city", "cities", "country", "countries", "building", "place", "state", "island", "location", "area", "site"]
 
 
 
 def analyze_question(question, choices):
     undercase_question = question.lower()
     q_words = undercase_question.split(" ")
-    word_types = pos_tag(word_tokenize(question.replace('"', "").replace("'", "").replace("following", "")))
+    word_types = pos_tag(word_tokenize(question.replace('"', "").replace("'s", "").replace("following", "")))
     print(word_types)
 
     # deciding by question type
-    if "who" in undercase_question or "whom" in undercase_question: # person question
+    if any(q_keyword in undercase_question for q_keyword in ["who", "whom", "whose"]): # person question
         print("Person Question Detected")
     elif "which" in undercase_question: # multiple selection question
-        extract_information("multi-selection", word_types, q_words)
+        extract_info_multi_selection(word_types, q_words)
         if "NOT" in q_words:
             pass
         else: 
@@ -25,41 +25,49 @@ def analyze_question(question, choices):
             if any(keyword in undercase_question for keyword in location_keywords) and any(keyword in undercase_question for keyword in location_keywords2):
                 pass
 
-def extract_information(q_type, word_types, q_words):
+
+    
+def extract_info_multi_selection(word_types, q_words):
     subject = ""
     condition = ""
 
-    if q_type == "multi-selection":
-        if q_words.index("which") != 0: # run if part of the condition is in front of the which statement
-            for i in range(0, q_words.index("which")):
-                if "of" in q_words[:q_words.index("which")]:
-                    if "IN" not in word_types[i][1] and "DT" not in word_types[i][1]:
-                        subject += word_types[i][0] + " "
-                else:
-                    condition += word_types[i][0] + " "
+    if q_words.index("which") > 1: # run if part of the condition is in front of the which statement
+        for i in range(0, q_words.index("which")):
+            curr_word = word_types[i][0]
+            if "of" in q_words[:q_words.index("which")]:
+                if "IN" not in word_types[i][1] and "DT" not in word_types[i][1] and curr_word != ",":
+                    subject += curr_word + " "
+            else:
+                if word_types[i][0] != ",":
+                    condition += curr_word + " "
 
-        curr_index = q_words.index("which") + 1
+    curr_index = q_words.index("which") + 1
+    curr_type = word_types[curr_index][1]
+    curr_word = word_types[curr_index][0]
+    subject_finished = False
+    subject_begun = False
+    while curr_index < len(word_types)-1:
+        if not subject_begun and any(grammer_type in curr_type for grammer_type in ["JJ", "NN", "CD"]):
+            subject_begun = True
+        if any(grammer_type in curr_type for grammer_type in ["VB", "RB", "MD"]) or curr_word == "for":
+            subject_finished = True
+        
+        if not subject_finished and subject_begun:
+            if curr_word != "one":
+                subject += curr_word + " "
+        elif subject_finished:
+            condition += curr_word + " "
+        curr_index += 1
         curr_type = word_types[curr_index][1]
-        subject_finished = False
-        subject_begun = False
-        while curr_index < len(word_types)-1:
-            if not subject_begun and any(grammer_type in curr_type for grammer_type in ["JJ", "NN", "CD"]):
-                subject_begun = True
-            if any(grammer_type in word_types[curr_index][1] for grammer_type in ["VB", "RB", "MD"]):
-                subject_finished = True
-            
-            if not subject_finished and subject_begun:
-                subject += word_types[curr_index][0] + " "
-            elif subject_finished:
-                condition += word_types[curr_index][0] + " "
-            curr_index += 1
-            curr_type = word_types[curr_index][1]
-            
-        print("Subject: " + subject)
-        print("Condition: " + condition)
+        curr_word = word_types[curr_index][0]
+    
+    if not subject:
+        subject = "-"
+    
+    return {"subject": subject, "condition": condition}
 
 
 
 
 
-analyze_question("""Of these three British islands, which is the largest in terms of land area?""", ["Menorca", "Ibiza", "La Palma"])
+analyze_question("""Which of these creatures has the fewest hearts?""", ["Menorca", "Ibiza", "La Palma"])
